3d663f433cea45b9fb6866e26b2ad6a531034276
ronah289 <ronald.kibet@student.moringaschool.com>
2021-11-06 15:39:49 -0800

    define database connection

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -1,2 +1,11 @@
-package models;public class DatabaseRule {
+package models;
+
+import org.junit.rules.ExternalResource;
+import org.sql2o.Sql2o;
+
+public class DatabaseRule extends ExternalResource{
+    @Override
+    public void before(){
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test","moringa","password");
+    }
 }
