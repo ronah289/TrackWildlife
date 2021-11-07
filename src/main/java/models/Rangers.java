ee85aa886e9325e51a1bcea38fd271646b61c738
ronah289 <ronald.kibet@student.moringaschool.com>
2021-11-07 11:14:07 -0900

    write tests for rangers class and implement

--- src/main/java/models/Rangers.java
+++ src/main/java/models/Rangers.java
@@ -1,4 +1,130 @@
 package models;
 
+import org.sql2o.Connection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Rangers {
+
+    private int id;
+    private String name;
+    private Integer staff_number;
+
+    public Rangers(String name, Integer staff_number) {
+        this.name = name;
+        this.staff_number = staff_number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getStaff_number() {
+        return staff_number;
+    }
+
+    public static List<Rangers> all(){
+        try (Connection con = DB.sql2o.open()){
+            String sql = "SELECT * FROM rangers";
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Rangers.class);
+
+        }
+
+    }
+
+    public void save(){
+        try (Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO rangers (name,staff_number) VALUES (:name,:staff_number)";
+            if(name.equals("")||staff_number.equals("")){
+                throw new IllegalArgumentException("All fields must be filled");
+            }
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("name",this.name)
+                    .addParameter("staff_number",this.staff_number)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static Rangers find(int id){
+        try (Connection con = DB.sql2o.open()){
+            String sql = "SELECT * FROM rangers WHERE id=:id";
+            return con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(Rangers.class);
+        }
+    }
+
+    public void update(int id,String name,String staff_number){
+        try (Connection con = DB.sql2o.open()){
+            String sql="UPDATE rangers SET name=:name,staff_number=:staff_number WHERE id=:id";
+            if(name.equals("")||staff_number.equals("")){
+                throw new IllegalArgumentException("Empty Fields Not Allowed");
+            }
+            con.createQuery(sql)
+                    .addParameter("id",this.id)
+                    .addParameter("name",name)
+                    .addParameter("staff_number",staff_number)
+                    .executeUpdate();
+        }
+    }
+
+    public void deleteOne(){
+        try (Connection con = DB.sql2o.open()){
+            String sql = "DELETE FROM rangers WHERE id=:id";
+
+            con.createQuery(sql)
+                    .addParameter("id",this.id)
+                    .executeUpdate();
+        }
+    }
+
+    public List<Sightings> getRangerSightings(){
+        try (Connection con = DB.sql2o.open()){
+            String sql = "SELECT sighting_id FROM rangers_sightings WHERE ranger_id=:ranger_id";
+            List<Integer> sightings_ids = con.createQuery(sql)
+                    .addParameter("ranger_id",this.getId())
+                    .executeAndFetch(Integer.class);
+            List<Sightings> sightings = new ArrayList<Sightings>();
+
+            for(Integer sighting_id:sightings_ids){
+                String sightingsQuery = "SELECT * FROM sightings WHERE id=:sighting_id";
+                Sightings sighting=con.createQuery(sightingsQuery)
+                        .addParameter("sighting_id",sighting_id)
+                        .executeAndFetchFirst(Sightings.class);
+                sightings.add(sighting);
+
+            }
+            if(sightings.size() == 0){
+                throw new IllegalArgumentException("No Sightings By This Ranger");
+            }
+            else {return sightings;}
+
+
+        }
+
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Rangers rangers = (Rangers) object;
+        return id == rangers.id &&
+                name.equals(rangers.name) &&
+                staff_number.equals(rangers.staff_number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, staff_number);
+    }
 }
