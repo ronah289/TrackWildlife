aeefe5a539ccfdface7eb17ac4f1cd4e591fb6b3
ronah289 <ronald.kibet@student.moringaschool.com>
2021-11-09 00:23:20 -0900

    prepare for deployment

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -3,5 +3,6 @@ package models;
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker","moringa","password");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker","moringa","password");
+public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-22-81-147.compute-1.amazonaws.com:5432/ddh272ov4eb7fh","uesggpomyombmm","0f23e2616fd0228fcea82d168a811f5a26e634811f966b159c7d8057d90544f1");
 }
